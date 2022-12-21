fc65d17d37f96809c8c2ced988cee8b3bb0c7b43
mahmoudhassanamin <117605440+mahmoudhassanamin@users.noreply.github.com>
2022-12-22 00:33:09 +0200

    update (update feature)

--- uptable.sh
+++ uptable.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-update=$(zenity --forms --title "update" --text "enter values" --add-entry "table name" --add-entry "attribute" --add-entry "value" --add-entry "where attribute" --add-entry "its value")
-tablename=$(echo update|cut -f1 -d|)
-att0=$(echo update|cut -f2 -d|)
-value=$(echo update|cut -f3 -d|)
-weratt=$(echo update|cut -f4 -d|)
-wervalue=$(echo update|cut -f5 -d|)
+update=$(zenity --forms --title "update" --text "enter values" --add-entry "table name" --add-entry "attribute" --add-entry "value" --add-entry "where attribute" --add-entry "its value" --separator=: )
+tablename=$(echo $update|cut -f1 -d:)
+att0=$(echo $update|cut -f2 -d:)
+value=$(echo $update|cut -f3 -d:)
+weratt=$(echo $update|cut -f4 -d:)
+wervalue=$(echo $update|cut -f5 -d:)
 if [ -f Databases/$DBconnect/"$tablename" ] && [[ $tablename != $rg2 && $tablename != "" ]]
 then
-att1=$(cut -f1 -d: Databases/$DBconnect/"meta$tablename"|grep -w "$att0")
-if [ $]
+att1=$(cut -f1 -d: Databases/$DBconnect/"meta$tablename"|grep -w "$weratt")
+att2=$(cut -f1 -d: Databases/$DBconnect/"meta$tablename"|grep -nw "$att0"|cut -f1 -d:)
+(( att2 ++ ))
+if [[ $weratt == $att1 ]]
 then
-
+#primary KEY check :)
+att1=$(cut -f1 -d: Databases/$DBconnect/"meta$tablename"|grep -nw "$weratt"|cut -f1 -d:)
+(( att1++ ))
+touch Databases/$DBconnect/temp
+awk -F: -v wat=$att1 -v at=$att2 -v v=":$value:" -v wv=$wervalue '{if($wat==wv){$at=v;print $0}else {print $0}}' Databases/$DBconnect/$tablename > Databases/$DBconnect/temp
+cp Databases/$DBconnect/temp Databases/$DBconnect/$tablename
+rm Databases/$DBconnect/temp
 else
 zenity --error --title "error" --text "attribute not found"
 fi
 else
-zenity --error --title "error" --text "table not found"
+zenity --error --title "error" --text "table not found OR invalid values"
 fi
 
